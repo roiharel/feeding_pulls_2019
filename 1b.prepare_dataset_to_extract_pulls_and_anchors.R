f06cd29cbe6a02cd5a87e1594d13708c1b31f046
Roiharel <roi.harel@mail.huji.ac.il>
2024-01-27 19:32:50 +0100

    1b_prepare_dataset to work with baboon data

--- 1b.prepare_dataset_to_extract_pulls_and_anchors.R
+++ 1b.prepare_dataset_to_extract_pulls_and_anchors.R
@@ -1,59 +1,66 @@
 library(lubridate)
 library(data.table)
-library(geosphere)
 library(dplyr)
 library(move)
+library(readr)
+
+library(geosphere)
+library(sf)
+library(rgdal)
+library(gdata)
 
 #### prepare dataset to extract pulls and anchors using eobs data
 
 #load movement dataset
-load("~/GPSdata.Rdata")
+#load("~/GPSdata.Rdata")
+setwd("~/GitHub/feeding_pulls_2019")
+load("~/GitHub/feeding_pulls_2019/data/baboon_data_2019.RData")
 
-a <-mp
-rm("mp")
-a <- plyr::compact(a) #removes null objects
+gps_v0 <- read_csv("data/Papio Anubis Mpala 2019_gps_raw_stage0.csv", 
+                   col_types = cols(`event-id` = col_skip(), 
+                                    visible = col_skip(), timestamp = col_datetime(format = "%Y-%m-%d %H:%M:%S"), 
+                                    `bar:barometric-pressure` = col_skip(), 
+                                    `data-decoding-software` = col_skip(), 
+                                    `eobs:activity` = col_skip(), `eobs:activity-samples` = col_skip(), 
+                                    `eobs:battery-voltage` = col_skip(), 
+                                    `eobs:fix-battery-voltage` = col_skip(), 
+                                    `eobs:key-bin-checksum` = col_skip(), 
+                                    `mag:magnetic-field-raw-x` = col_skip(), 
+                                    `mag:magnetic-field-raw-y` = col_skip(), 
+                                    `mag:magnetic-field-raw-z` = col_skip(), 
+                                    `quaternion-raw-w` = col_skip(), 
+                                    `quaternion-raw-x` = col_skip(), 
+                                    `quaternion-raw-y` = col_skip(), 
+                                    `quaternion-raw-z` = col_skip(), 
+                                    `sensor-type` = col_skip(), `individual-taxon-canonical-name` = col_skip(), 
+                                    `study-name` = col_skip()))
+View(gps_v0)
+gps_v0 <- as.data.frame(gps_v0)
+gps_v0 <- plyr::compact(gps_v0) #removes null objects
 
-for (i in 1:length(a)){
-  options(digits=10)
-  w <- as.data.frame(a[[i]])
-  w <- w[,c("local_identifier", "location_lat", "location_long", "timestamps")]
-  a[[i]] <- w
-  rm(w)
-} #this for loop reduces the size of the list -by only keeping the useful columns- which is massive otherwise
+gps_v0 <- gps_v0 %>%
+  select(`tag-local-identifier`, `location-lat`, `location-long`, `timestamp`)
 
-library (plyr)
-d <- ldply (a, data.frame)
-rm(a)
+gps_v0 <- gps_v0 %>%
+  filter(!if_any(everything(), is.na))
 
 
 #exclude outliers outside the extended study area
-data <- d[which(d$location_lat > 0.260464 & d$location_lat < 0.324190 & d$location_long < 36.926129 & d$location_long >36.873491),]
-rm("d")
-
-library(sp)
-library(rgdal)
-library(gdata)
+#gps_v0 <- gps_v0[which(gps_v0$'location-lat' > 0.260464 & gps_v0$'location-lat' < 0.324190 & gps_v0$'location-long' < 36.926129 & gps_v0$'location-long' >36.873491),]
 
 #   transform positional data to utm values
 # distance will therefore be in meters
 # geological position of Mpala Kenya in UTM
 projectionWant <- "+proj=utm +zone=37 +ellps=WGS84" 
-coordinates(data) <- c("location_long",  "location_lat")
-proj4string(data) <- CRS("+proj=longlat +datum=WGS84")
+coordinates(gps_v0) <- c("location-long",  "location-lat")
+proj4string(gps_v0) <- CRS("+proj=longlat +datum=WGS84")
 
-data2 <- spTransform(data, CRSobj=projectionWant)
+gps_v0 <- spTransform(gps_v0, CRSobj=projectionWant)
 
 #   variables needed
-ids <- unique(data2$local_identifier)
+ids <- unique(gps_v0$'tag-local-identifier')
 
-#   positional matrix of all individuals
-# converting individual position from movebank object
-# into two matrices with each individual position 
-#
-####
-
-# timeline organisation and matrices
-days <- unique(as.Date(data2$timestamps))
+days <- unique(as.Date(gps_v0$timestamp))
 hours <- seq(3,16,1)
 minutes <- seq(0,59,1)
 seconds <- seq(0,59,1)
@@ -63,24 +70,36 @@ times <- paste(times$day," ",times$hour,":",times$minute,":",times$second,sep=""
 times <- sort(strptime(times, format="%Y-%m-%d %H:%M:%S", tz="UTC"))
 times <- as.POSIXct(times)
 
-#save(times, file="~/times.Rdata")
 
-rm(list=setdiff(ls(), c("times", "ids", "data2")))
-
+#   positional matrix of all individuals
+# converting individual position from movebank object
+# into two matrices with each individual position 
+#
 xs <- ys <- matrix(NA, nrow = length(ids), ncol = length(times))
 
 # fill matrices with data from data2 for all individuals
 for (i in 1:length(ids)) {
-  a <- data2[which(data2$local_identifier == ids[i]),]
-  xs[i,] <- a@coords[,1][match(times, a$timestamps)]
-  ys[i,] <- a@coords[,2][match(times, a$timestamps)]
+  a <- gps_v0[which(gps_v0$'tag-local-identifier' == ids[i]),]
+  xs[i,] <- a@coords[,1][match(times, a$timestamp)]
+  ys[i,] <- a@coords[,2][match(times, a$timestamp)]
 }
 rownames(xs) <- ids
 rownames(ys) <- ids
 
 
+
+
+
+
+
+# timeline organisation and matrices
+
+save(times, file="~/times.Rdata")
+
 save(xs, file="~/x_axis.Rdata")
 save(ys, file="~/y_axis.Rdata")
+
+rm(list=setdiff(ls(), c("times", "ids", "data2")))
 
 
 #first timestamp of each day
